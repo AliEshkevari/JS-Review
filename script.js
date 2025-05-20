3334930e16b43e4bbfc58c4cb8dcfcda3dac296b
Ali <alieshkevari1908@gmail.com>
2025-05-20 22:08:58 +0330

    Implemented a tiny function

--- script.js
+++ script.js
@@ -24,3 +24,28 @@ const movieDetails = movies.map((movie) => ({
 }));
 
 // console.log(movieDetails)
+
+
+
+const ratingIsEqual = (movie1Name, movie2Name) =>{
+  // if((!(movies.map(movie => movie.name)).includes(movie1)) && (!(movies.map(movie => movie.name)).includes(movie2))) {
+  //   return 'Movie doesn\'t exist'
+  // } else {
+  //   console.log(movie1.rating, movie2)
+  //   return movie1.rating === movie2.rating
+  // }
+
+  let movie1 = movies.find((movie) => movie.name === movie1Name)
+  let movie2 = movies.find((movie) => movie.name === movie2Name)
+
+  console.log(movie1.rating)
+  console.log(movie2.rating)
+  // (!movie1 || !movie2) ? 'Movie doesn\t exist' : movie1.rating === movie2.rating
+
+  if(!movie1 || !movie2) return 'Movie doesn\t exist'
+  return movie1.rating === movie2.rating
+
+}
+
+// console.log(movies.map(movie => movie.name))
+console.log(ratingIsEqual('Amelie', 'Inception'))
