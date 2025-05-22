39b9a437e0af2e8e5025604c381cae149cc42f51
Ali <alieshkevari1908@gmail.com>
2025-05-22 15:44:19 +0330

    added movie handling methods & some minor functions

--- script.js
+++ script.js
@@ -48,4 +48,145 @@ const ratingIsEqual = (movie1Name, movie2Name) =>{
 }
 
 // console.log(movies.map(movie => movie.name))
-console.log(ratingIsEqual('Amelie', 'Inception'))
+// console.log(ratingIsEqual('Amelie', 'Inception'))
+
+
+// average rating
+let avgRating = movies.reduce((sum, movie) => sum + movie.rating, 0) / movies.length
+
+// console.log('Average rating is', avgRating.toFixed(2))
+
+
+const topRatedMovies = movies.sort((a, b) => b.rating - a.rating).map((movie) => ({
+  name: movie.name,
+  rating: movie.rating
+}))
+
+// console.log(topRatedMovies)
+
+
+const topRatedByGenre = (genre) => {
+  let topRatedMoviesByGenre = movies.filter(movie => movie.genre === genre).sort((a,b) => b.rating - a.rating).map((movie) => ({
+    name: movie.name,
+    rating: movie.rating
+  }))
+
+  const list = topRatedMoviesByGenre.map(movie => `${movie.name}: ${movie.rating}`).join('\n')
+
+  return `Top rated ${genre} movies are as follows: \n${list}`
+}
+
+
+let topRatedDramaMovies = movies.filter(movie => movie.genre === 'Drama').sort((a,b) => b.rating - a.rating).map((movie) => ({
+  name: movie.name,
+  rating: movie.rating
+}))
+
+// console.log('Top rated drama movies are as follows: ', topRatedDramaMovies)
+// console.log(topRatedByGenre('Drama'))
+
+
+// didn't work:
+// const moviesGroupedByGenre = (movies) => {
+//   const result = [{}]
+//   for(let movie of movies) {
+//     let genre = movie.genre
+//     result[0][genre] = [...result[0][genre], movie]
+//   }
+//   return result
+// }
+
+// console.log(moviesGroupedByGenre(movies))
+
+
+
+const moviesGroupedByGenre = (movies) => {
+  let result = Object.groupBy(movies, movie => movie.genre)
+  return result
+}
+
+// console.log(moviesGroupedByGenre(movies))
+
+
+
+const findEqualRatedMovies = (movies, movieName) => {
+  let targetMovie = movies.find(movie => movie.name === movieName)
+  if(!targetMovie) return `Movie with name ${movieName} not found. try another one`
+
+  let equalRatedMovies = movies.filter(movie => (movie.name !== movieName) && movie.rating === targetMovie.rating).map(movie => `${movie.name}`)
+
+  return `Movies with a rating equal to ${movieName} with rating ${targetMovie.rating} movie are: \n${equalRatedMovies.join('\n')}`
+}
+
+// console.log(findEqualRatedMovies(movies, 'Inception'))
+
+
+
+
+async function addMovie(name, year, director, genre, rating){
+  try{
+    let newMovie = {name, year, director, genre, rating}
+    let res =  await fetch('http://localhost:3000/movies', {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newMovie)
+    })
+    if(!res.ok) throw new Error('Movie not added.')
+    let addedMovie = await res.json()
+    return addedMovie
+  } catch(err){
+    console.error('Movie not added.', err.message)
+    return
+  }
+}
+
+
+// addMovie('12 angry men', 1957, "Sydney Lumet", "Drama", 9.0)
+
+
+async function deleteMovie(movieId){
+  try{
+    let res = await fetch(`http://localhost:3000/movies/${movieId}`, {
+      method: 'DELETE'
+    })
+    if(!res.ok) throw new Error('Movie not deleted.')
+    let deletedMovie = await res.json()
+  console.log(deletedMovie)
+  return true
+  } catch(err){
+    console.error('Movie not deleted.', err.message)
+    return
+  }
+}
+
+// deleteMovie("f198")
+// deleteMovie("8a75")
+
+
+async function updateMovie(movieId, info){
+  try{
+    let res = await fetch(`http://localhost:3000/movies/${movieId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(info)
+    })
+    if(!res.ok) throw new Error('Movie not updated.')
+    let updatedMovie = await res.json()
+    return updatedMovie
+  } catch(err){
+    console.error('Movie not updated.', err)
+    return
+  }
+}
+
+// updateMovie(1, {
+//   name: 'Dancer in the dark (v2)',
+//   rating: 9.1
+// })
+
+
+
